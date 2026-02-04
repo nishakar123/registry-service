5fcc779d1866cd777d21e022aeaaa7e8d6f0b957
nishakar123 <nishakar123@gmail.com>
2026-02-04 18:30:03 +0530

    Initial commit

---
+++ src/main/java/com/nishakar/RegistryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.nishakar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RegistryServiceApplication.class, args);
+	}
+
+}
